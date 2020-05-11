15820ecdec0bbbfdf49eaaa9b843a441e0f439e7
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-05-10 21:00:37 -0700

    Create BFS.java

---
+++ BFS.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class BFS {
+  // Do not edit the class below except
+  // for the breadthFirstSearch method.
+  // Feel free to add new properties
+  // and methods to the class.
+  static class Node {
+    String name;
+    List<Node> children = new ArrayList<Node>();
+
+    public Node(String name) {
+      this.name = name;
+    }
+
+    public List<String> breadthFirstSearch(List<String> array) {
+      // Write your code here.
+			Queue<Node> queue = new LinkedList<Node>();
+			queue.add(this);
+			while(!queue.isEmpty()){
+				Node current = queue.poll();
+				array.add(current.name);
+				queue.addAll(current.children);
+			}
+      return array;
+    }
+
+    public Node addChild(String name) {
+      Node child = new Node(name);
+      children.add(child);
+      return this;
+    }
+  }
+}
